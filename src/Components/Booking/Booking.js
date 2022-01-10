f43c51803864b5098353d174b67dedbd01ed3137
jahidshawon19 <jahidshawon1730@gmail.com>
2022-01-10 11:19:46 +0600

    send booking information to the database

--- src/Components/Booking/Booking.js
+++ src/Components/Booking/Booking.js
@@ -1,16 +1,98 @@
-import React from 'react';
+import React, { useRef  } from 'react';
 import useAuth from '../../hooks/useAuth';
 import Footer from '../Shared/Footer/Footer';
 import Navbar from '../Shared/Navbar/Navbar';
 import './Booking.css'
 const Booking = () => {
     const {user} = useAuth()
+   
+
+    const senderNameRef = useRef()
+    const senderEmailRef = useRef()
+    const senderMobileRef = useRef()
+    const senderAddressRef = useRef()
+    const pickupPlaceRef = useRef()
+    const dateRef = useRef()
+
+
+    const recipentNameRef = useRef()
+    const recipientEmailRef = useRef() 
+    const recipientMobileRef = useRef()
+    const recipentDistrictRef = useRef()
+    const destinationRef = useRef()
+
+
+
+    const parcelTypeRef = useRef()
+    const parcelContainRef = useRef()
+    const amountRef = useRef()
+
+
+
+
+    const handleBooking = e =>{
+      const senderName = senderNameRef.current.value 
+      const senderEmail = senderEmailRef.current.value 
+      const senderMobile = senderMobileRef.current.value 
+      const senderAddress = senderAddressRef.current.value 
+      const pickupPlace = pickupPlaceRef.current.value 
+      const date = dateRef.current.value 
+
+
+      const recipientName = recipentNameRef.current.value 
+      const recipientEmail = recipientEmailRef.current.value 
+      const recipientMobile = recipientMobileRef.current.value 
+      const recipientDistrict = recipentDistrictRef.current.value
+      const destination = destinationRef.current.value 
+
+      const parcelType = parcelTypeRef.current.value 
+      const parcelContain = parcelContainRef.current.value
+      const amount = amountRef.current.value 
+      
+
+      const newBooking = {senderName, senderEmail, senderMobile, senderAddress, pickupPlace, date, recipientName, recipientMobile,recipientEmail, recipientDistrict, destination,parcelType, parcelContain,amount}
+
+      
+      fetch('http://localhost:5000/bookings', {
+          method: 'POST',
+          headers:{
+            'content-type': 'application/json'
+          },
+          body:JSON.stringify(newBooking)
+      })
+
+          .then(res => res.json())
+          .then(data => {
+            if(data.insertedId){
+              alert('We Recevied Your Booking. We Will Contact With You Soon. Thank You.')
+          }
+          })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      e.preventDefault()
+    }
+  
+
     return (
 
         <>
         <Navbar></Navbar>
         <div className="container">
-            <form action="">
+            <form action="" onSubmit={handleBooking}>
             <div className="row">
             
             <div className="col-lg-6">
@@ -31,7 +113,7 @@ const Booking = () => {
                                <label className="order-form-label">SenderName</label>
                              </div>
                              <div className="col-12 ">
-                               <input className="order-form-input" value={user.displayName} />
+                               <input className="order-form-input" ref={senderNameRef} value={user.displayName} />
                              </div>
                             
                            </div>
@@ -41,7 +123,7 @@ const Booking = () => {
                                <label className="order-form-label">Sender Email</label>
                              </div>
                              <div className="col-12">
-                               <input className="order-form-input" value={user.email} />
+                               <input className="order-form-input"  ref={senderEmailRef} value={user.email} />
                              </div>
                            </div>
                  
@@ -50,7 +132,7 @@ const Booking = () => {
                                <label className="order-form-label">Sender Mobile</label>
                              </div>
                              <div className="col-12">
-                               <input className="order-form-input" placeholder=" " />
+                               <input className="order-form-input"  ref={senderMobileRef} placeholder=" " />
                              </div>
                            </div>
 
@@ -61,7 +143,7 @@ const Booking = () => {
                                <label className="order-form-label">Pickup Place</label>
                              </div>
                              <div className="col-12">
-                               <input className="order-form-input" placeholder=" " />
+                               <input className="order-form-input" ref={pickupPlaceRef} placeholder=" " />
                              </div>
                            </div>
                  
@@ -70,7 +152,7 @@ const Booking = () => {
                                <label className="order-form-label" for="date-picker-example">Pickup Date</label>
                              </div>
                              <div className="col-12">
-                               <input className="order-form-input datepicker" placeholder="Selected date" type="date"
+                               <input className="order-form-input datepicker"  ref={dateRef} placeholder="Selected date" type="date"
                                 />
                              </div>
                            </div>
@@ -79,7 +161,7 @@ const Booking = () => {
                                <label className="order-form-label">Sender Address</label>
                              </div>
                              <div className="col-12">
-                               <textarea className="order-form-input" placeholder=" " />
+                               <textarea className="order-form-input" ref={senderAddressRef} placeholder=" " />
                              </div>
                            </div>
                        
@@ -112,7 +194,7 @@ const Booking = () => {
                                <label className="order-form-label">Recipient Name</label>
                              </div>
                              <div className="col-12 ">
-                               <input className="order-form-input" placeholder="Full Name" />
+                               <input className="order-form-input"  ref={recipentNameRef} placeholder="Full Name" />
                              </div>
                             
                            </div>
@@ -122,7 +204,7 @@ const Booking = () => {
                                <label className="order-form-label">Recipient Email</label>
                              </div>
                              <div className="col-12">
-                               <input className="order-form-input" placeholder=" " />
+                               <input className="order-form-input"  ref={recipientEmailRef} placeholder=" " />
                              </div>
                            </div>
                  
@@ -131,7 +213,7 @@ const Booking = () => {
                                <label className="order-form-label">Recipient Mobile</label>
                              </div>
                              <div className="col-12">
-                               <input className="order-form-input" placeholder=" " />
+                               <input className="order-form-input" ref={recipientMobileRef} placeholder=" " />
                              </div>
                            </div>
 
@@ -143,7 +225,13 @@ const Booking = () => {
                                <label className="order-form-label">Recipient District</label>
                              </div>
                              <div className="col-12">
-                               <input className="order-form-input" placeholder=" " />
+                               
+                               <select className="order-form-input" ref={recipentDistrictRef} aria-label="Default select example">
+                                <option selected>District</option>
+                                <option value="1">Dhaka</option>
+                                <option value="2">Feni</option>
+                                <option value="3">Chittagong</option>
+                              </select>
                              </div>
                            </div>
                  
@@ -153,7 +241,7 @@ const Booking = () => {
                                <label className="order-form-label">Destination Address</label>
                              </div>
                              <div className="col-12">
-                               <textarea className="order-form-input" placeholder=" " />
+                               <textarea className="order-form-input" ref={destinationRef} placeholder=" " />
                              </div>
                            </div>
                        
@@ -186,7 +274,7 @@ const Booking = () => {
                                <label className="order-form-label">Parcel Type</label>
                              </div>
                              <div className="col-12 ">
-                               <input className="order-form-input" />
+                               <input className="order-form-input"  ref={parcelTypeRef} />
                              </div>
                             
                            </div>
@@ -196,7 +284,7 @@ const Booking = () => {
                                <label className="order-form-label">What does this parcel contain?</label>
                              </div>
                              <div className="col-12">
-                               <input className="order-form-input" placeholder=" " />
+                               <input className="order-form-input" ref={parcelContainRef} placeholder=" " />
                              </div>
                            </div>
                  
@@ -205,14 +293,14 @@ const Booking = () => {
                                <label className="order-form-label">Number of Item</label>
                              </div>
                              <div className="col-12">
-                               <input className="order-form-input" type="number" placeholder=" " />
+                               <input className="order-form-input"  ref={amountRef} type="number" placeholder=" " />
                              </div>
                            </div>
 
        
                            <div className="row mt-3">
                              <div className="col-12">
-                               <button type="button" id="btnSubmit" className="btn btn-dark d-block mx-auto btn-submit">Confirm Booking</button>
+                               <button type="submit" id="btnSubmit" className="btn btn-dark d-block mx-auto btn-submit">Confirm Booking</button>
                              </div>
                            </div>
                  
